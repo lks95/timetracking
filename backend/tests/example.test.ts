e166e71b1e4fadf34e98b5a8b37e28834e74ea28
lk104 <lk104@hdm-stuttgart.de>
2020-05-20 14:15:05 +0200

    added test class
    for:
    /projects/{id}/tasks/{task-id}
    /projects/{id}/start
    /projects/{id}/stop

--- backend/tests/example.test.ts
+++ backend/tests/example.test.ts
@@ -13,16 +13,3 @@ describe('OK', () => {
     expect(result).to.not.equal('NOT OK');
   });
 });
-
-describe('Task', () => {
-  const task = new Task();
-  it('should be an object', () => {
-    expect(task).to.be.an('object');
-  });
-  it('should be by default not completed', () => {
-    expect(task.isCompleted()).to.be.false;
-  });
-  it('should be by default not completed', () => {
-    expect(task.getDescription()).to.be.empty;
-  });
-});
