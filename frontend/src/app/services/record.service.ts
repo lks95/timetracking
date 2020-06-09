9ed4a07fa834994c4a4a0162c2b4c6050df6d175
Christos Malliaridis <cm116@hdm-stuttgart.de>
2020-06-09 19:54:52 +0200

    Remove dummy data

--- frontend/src/app/services/record.service.ts
+++ frontend/src/app/services/record.service.ts
@@ -2,10 +2,8 @@ import {Injectable} from '@angular/core';
 import {Record} from '../models/record';
 import {Project} from '../models/project';
 import {Task} from '../models/task';
-import {RECORDS} from './mock-records';
-import {HttpClient, HttpErrorResponse} from '@angular/common/http';
-import {BehaviorSubject, Observable, ObservableInput, Observer, of, Subject, throwError} from 'rxjs';
-import {catchError, retry, share} from 'rxjs/operators';
+import {HttpClient} from '@angular/common/http';
+import {Observable, of} from 'rxjs';
 import { environment } from '../../environments/environment';
 
 const apiUrl = environment.apiUrl;
@@ -16,10 +14,6 @@ const apiUrl = environment.apiUrl;
 export class RecordService {
 
   constructor(private httpClient: HttpClient) { }
-
-  getRecords(): Observable<Record[]> {
-    return of(RECORDS);
-  }
 
   startProjectRecording(element: Project): Observable<Record> {
     return this.httpClient.post<Record>(apiUrl + 'records/start', {
--- frontend/src/app/services/task.service.ts
+++ frontend/src/app/services/task.service.ts
@@ -1,7 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Task } from '../models/task';
-import { TASKS } from './mock-tasks';
-import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +6,5 @@ import { Observable, of } from 'rxjs';
 export class TaskService {
 
   constructor() { }
-
-  getTasks(): Observable<Task[]> {
-    return of(TASKS);
-  }
 
 }
