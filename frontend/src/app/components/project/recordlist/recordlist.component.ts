03a5cd510ec394c217c67a41b3ace65c616f3dd0
Christos Malliaridis <cm116@hdm-stuttgart.de>
2020-06-13 14:10:00 +0200

    Update record list to show task records too

--- frontend/src/app/components/project/recordlist/recordlist.component.ts
+++ frontend/src/app/components/project/recordlist/recordlist.component.ts
@@ -1,25 +1,56 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {Record} from '../../../models/record';
 import {RecordService} from '../../../services/record.service';
 import {MatDialog} from '@angular/material/dialog';
 import {EditRecordDialog} from '../../dialogs/edit-record/edit-record.dialog';
+import {Project} from '../../../models/project';
+import {Task} from '../../../models/task';
+import {ProjectService} from '../../../services/project.service';
+import {Subscription} from 'rxjs';
+import {TaskService} from '../../../services/task.service';
 
 @Component({
   selector: 'app-recordlist',
   templateUrl: './recordlist.component.html',
   styleUrls: ['./recordlist.component.scss']
 })
-export class RecordlistComponent implements OnInit {
+export class RecordlistComponent implements OnInit, OnDestroy {
 
+  currentProject: Project;
+  selectedTask: Task;
   @Input() records: Record[] = [];
 
+  projectSelectionSub: Subscription;
+  taskSelectionSub: Subscription;
+
   constructor(
+    private projectService: ProjectService,
+    private taskService: TaskService,
     private recordService: RecordService,
     private dialog: MatDialog
   ) {
   }
 
   ngOnInit(): void {
+    this.subscribeToObservables();
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe();
+  }
+
+  subscribeToObservables(): void {
+    this.projectSelectionSub = this.projectService.onProjectSelection.subscribe(project => {
+      this.currentProject = project;
+    });
+    this.taskSelectionSub = this.taskService.onTaskSelection.subscribe(task => {
+      this.selectedTask = task;
+    });
+  }
+
+  unsubscribe(): void {
+    this.projectSelectionSub.unsubscribe();
+    this.taskSelectionSub.unsubscribe();
   }
 
   openEditRecordDialog(record: Record): void {
@@ -34,5 +65,10 @@ export class RecordlistComponent implements OnInit {
     dialogRef.afterClosed().subscribe(result => {
       // this.selectedProject = result;
     });
+  }
+
+  getTaskDescription(record: Record): string {
+    const task = (this.currentProject?.tasks as Task[]).filter(t => t._id === record.task);
+    return task[0]?.description;
   }
 }
